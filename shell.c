af63e5f2efe6bfdbbb2b3370ed21447db28c87e4
Benjamin Goihman <bengoihman@gmail.com>
2024-12-02 14:39:46 -0500

    add parse args form old lab

--- shell.c
+++ shell.c
@@ -8,3 +8,10 @@ int main(){
     printf("hello world\n");
     return 0;
 }
+
+
+void parse_args(char* line, char ** arg_ary){
+    //Parses single command in-memory
+    int i = 0;
+    while((arg_ary[i] = strsep( &line, " " ))){ i++;}
+}
