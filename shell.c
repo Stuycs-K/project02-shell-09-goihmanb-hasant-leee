1f630a3c12ab72305c9feff8ceb45a6ca6e780ce
Tanzeem Hasan <thasan50@stuy.edu>
2024-12-03 14:52:02 -0500

    Merge conflict?

--- shell.c
+++ shell.c
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[]){
     printf("hello world\n");
+
     return 0;
 }
 
@@ -14,4 +15,14 @@ void parse_args(char* line, char ** arg_ary){
     //Parses single command in-memory
     int i = 0;
     while((arg_ary[i] = strsep( &line, " " ))){ i++;}
+}
+
+void redirect_output(char * fileName, char * output) {
+  //going from file into standard output?
+  int fd1 = open(fileName, O_WRONLY); //opens a file for storing output
+  // if file doesn't exist, make one? Or maybe that should be in main
+  int FILENO = stdout; //stores standard output
+  int backup_stdout = dup(FILENO); //also stores standard output
+  dup2(fd1, FILENO); //redirects the file content to standard output
+  //write(fd1, &FILENO, sizeof(int));
 }
