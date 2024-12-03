ecc00aa14fe52a9611faf93d749f99c863eaeb60
OohBen <89159600+OohBen@users.noreply.github.com>
2024-12-03 18:58:27 -0500

    Execute first commnad (:

--- shell.c
+++ shell.c
@@ -17,6 +17,7 @@ void get_cmds(char** cmds){
     char line_buff[1000];
     printf("enter command: ");
     fgets(line_buff, sizeof(line_buff), stdin);
+    line_buff[strcspn(line_buff, "\n")] = 0; //Removes newline breaks code
     char *curr = line_buff;
     char * token;
     token = strsep( &curr, ";" );
@@ -37,5 +38,20 @@ int main(int argc, char *argv[]) {
         printf("%s\n",cmds[i]);
         i++;
     }
+    char *home = getenv("HOME");
+    printf("Home: %s\n", home);
+    chdir(home);
+    char *arg_ary[1000];
+    parse_args(cmds[0], arg_ary);
+    printf("arg_ary[0]: %s\n", arg_ary[0]);
+    printf("arg_ary[1]: %s\n", arg_ary[1]);
+    int pid = fork();
+    if (pid == 0){
+        execvp(arg_ary[0], arg_ary);
+    }
+    else{
+        wait(NULL);
+    }
+
     return 0;
 }
